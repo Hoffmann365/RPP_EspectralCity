0aa3afd32b4ad3fda5b33089072b26d78e493e5b
Davi Hoffmann <daviwhagner@gmail.com>
2024-09-11 15:09:01 -0300

    Singleton Implementado

---
+++ EspectralCity/Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    
+    private void Awake()
+    {
+        //Singleton
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
